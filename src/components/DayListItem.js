5a400325e25141d8fd9551e74d0090cca7f9a684
Simran Kalirai <kaliraisimran@gmail.com>
2022-10-04 16:53:45 -0700

    buttons added + Daylist functionality almost complete

---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
+
+export default function DayListItem(props) {
+  const selectIsTrue = props.selected;
+  console.log(props.spots);
+  function formatSpots() {
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    } else if(props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return "no spots remaining";
+    }
+  }
+  const text = formatSpots();
+  const fullIsTrue = props.spots === 0;
+  const checkIfTrue = classNames("day-list__item", {"day-list__item--selected":selectIsTrue}, {"day-list__item--full":fullIsTrue});
+  return (
+    <li onClick={() => {props.setDay(props.name)}} className={checkIfTrue}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light" >{text}</h3>
+    </li>
+  );
+}
