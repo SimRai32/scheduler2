3280b81a9bcb4376aea22b303a1fa71cf878d66c
Simran Kalirai <kaliraisimran@gmail.com>
2022-10-05 15:32:43 -0700

    On this commit the Appointment component will have all but the form component functional

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,11 @@ import Button from "components/Button";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Delete from "components/Appointment/Delete";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -142,3 +147,40 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm"/>)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+    <Show 
+      onEdit={action("onEdit")} 
+      onDelete={action("onDelete")}
+      />
+    ))
+  .add("Confirm", () => (
+    <Confirm 
+      message="Delete the appointment?" 
+      onConfirm={action("onConfirm")} 
+      onCancel={action("onCancel")}
+      />
+    ))
+  .add("Delete", () => <Delete message="Deleting"/>)
+  .add("Error", () => (
+    <Error 
+      message="Could not delete appointment." 
+      onClose={action("onClose")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      student="Sim"
+      interviewer={4}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  
