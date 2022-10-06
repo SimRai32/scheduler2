ca624557928f4aab73af0ccbb6866d60ad2a5127
Simran Kalirai <kaliraisimran@gmail.com>
2022-10-05 18:04:57 -0700

    On this commit the schedule shows the created appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,19 @@
 import React from "react";
-import "./styles.scss"
+import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 export default function Appointment(props) {
+  const interview = props.interview;
+  console.log(props);
+  const render = (
+    interview ? <Show student={interview.student} interviewer={interview.interviewer.name}/> 
+    : <Empty/>
+    )
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {render}
+    </article>
   )
 }
