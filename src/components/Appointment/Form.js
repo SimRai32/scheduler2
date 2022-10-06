ec5780b6877949c742f5d2b5ffaa9f70821e10b2
Simran Kalirai <kaliraisimran@gmail.com>
2022-10-05 17:23:37 -0700

    On this commit the form cancel button and basic creation features will work

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,31 +1,45 @@
 import React from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
+import { useState } from 'react';
 
 export default function Form (props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+  
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          value={interviewer}
+          interviewers={props.interviewers}
+          onChange={(set)=>{setInterviewer(set)}}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button onClick={cancel} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
         </section>
       </section>
     </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,6 +3,7 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
+  console.log("List props", props);
   const interviewers  = props.interviewers;
   const value = props.value;
   const onChange = props.onChange;
